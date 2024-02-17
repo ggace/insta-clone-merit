5b000b4dd9f42f0e5fb815777e903a646c4daeed
ggace <skydream5485@gmail.com>
2024-02-17 18:33:28 +0900

    sql 외부파일화 및 숨기기

--- database/FeedDatabase.py
+++ database/FeedDatabase.py
@@ -1,41 +1,17 @@
 import database.connection as database
+import database.sqls as sqls
 
 def profile(user_id):
-    sql = f'''
-SELECT feeds.id, feeds.content, media.media AS first_media, users.user_id, users.nickname, users.description, users.profile_img
-FROM feeds 
-INNER JOIN media 
-INNER JOIN users 
-ON media.feed_id = feeds.id 
-AND users.id=feeds.owner_id 
-WHERE users.id={user_id}
-GROUP BY feeds.id
-        '''
+    sql = sqls.get_sql_about_profile(user_id)
     
     return database.sqlRun(sql)
 
 def search_feeds_count_by_similar_tag(tag):
-    sql = f"""
-SELECT tag, COUNT(DISTINCT feed_id) AS `count`
-FROM feed_tag
-WHERE tag LIKE "%{tag}%"
-GROUP BY tag
-"""
+    sql = sqls.get_sql_about_search_feeds_count_by_similar_tag(tag)
     
     return database.sqlRun(sql)
 
 def search_feeds_by_tag(tag):
-    sql = f"""
-SELECT feeds.id, feeds.content, media.media AS first_media, IF(users.nickname=NULL, users.nickname, users.user_id) as user, users.profile_img
-FROM feeds 
-INNER JOIN media 
-INNER JOIN users 
-INNER JOIN feed_tag
-ON media.feed_id = feeds.id 
-AND feed_tag.feed_id = feeds.id
-AND users.id=feeds.owner_id
-WHERE feed_tag.tag = "{tag}"
-GROUP BY feeds.id
-"""
+    sql = sqls.get_sql_about_search_feeds_by_tag(tag)
     
     return database.sqlRun(sql)
